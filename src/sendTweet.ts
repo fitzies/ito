13f695ebfbc56cf441dbfe6147e70c93f9b89b76
fitzies <olicrypt@gmail.com>
2025-03-27 13:03:11 +0800

    tweet

--- src/sendTweet.ts
+++ src/sendTweet.ts
@@ -1,10 +1,10 @@
 import { getAndUpdateSource } from "./lib/db";
 import { generate, sendTelegram, tweet } from "./lib/utils";
 
-export const teleAndTweet = async () => {
+export const sendTweet = async () => {
   const source = await getAndUpdateSource(
-    { tweeted: false, messaged: false },
-    { tweeted: true, messaged: true }
+    { tweeted: false },
+    { tweeted: true }
   );
   if (!source) {
     console.log("Error getting source");
@@ -14,11 +14,8 @@ export const teleAndTweet = async () => {
   const tweetText = await generate(
     `${source.content}\n\nUse this news article to create a tweet. Make the tweet about 30 words. use a hashtag if a product/company/etc and include statstics if any.`
   );
-  const tele = await generate(
-    `${source.content}\n\nUse this text and give me a 100 word article about it for telegram.`
-  );
-  await sendTelegram(tele);
+
   await tweet(tweetText);
 };
 
-await teleAndTweet();
+await sendTweet();
